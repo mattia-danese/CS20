df591d89d674c8d88dcd84e680ae1bcd56895a46
Mattia Danese <mattiadanese@Mattias-MacBook-Air.local>
2022-03-10 10:15:05 -0500

    hw1-5, 9

---
+++ hw9/script.js
@@ -0,0 +1,42 @@
+function initialize(){
+    str =  JSON.stringify(data);
+    document.getElementById("raw").innerHTML = str;
+
+    let niceOutput = function () {
+        let songs = data["Songs"];
+        let output = "";
+        for(i=0; i < songs.length; i++){
+            output += "<div>";
+            let song = songs[i];
+            
+            output += "Title: " + song["title"] + "<br>";
+            output += "Artists: " + song["artists"].join(' ') + "<br>";
+            output += "Genres: " + song["genres"].join(', ') + "<br>";
+            output += "Released: " + song["year"] + "<br>";
+            output += "<br>";
+            output += "</div>";
+        }
+
+        document.getElementById("nice").innerHTML = output;
+    }
+    niceOutput();
+}
+
+function filter() {
+    let genre = document.getElementById("genres").value;
+    let songs = data["Songs"];
+    let output = "";
+
+    for(i=0; i < songs.length; i++){
+        output += "<div>";
+        let song = songs[i];
+
+        if(song.genres.includes(genre)){
+            output += song["title"] + "<br>";
+        }
+        output += "</div>";
+    }
+
+    document.getElementById("filter").innerHTML = output;
+
+}
